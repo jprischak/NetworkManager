39475195c0e31458d459116664e57b8b58748db7
JASONLAPTOP\Jason <jprischak@gmail.com>
2018-03-07 20:18:48 -0500

    Player objects added, manager script written, player script control written.

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+    /**
+    *  VARIABLES
+    * */
+    public GameObject playerPrefab;
+
+
+    private const string TYPENAME = "UniqueGame";
+    private const string GAMENAME = "RoomName";
+    private HostData[] hostList;
+
+
+
+
+
+    /**
+    *  CLASS FUNCTIONS
+    * */
+
+    // This function is called when the server is successfully initialized
+    private void OnServerInitialized()
+    {
+        Debug.Log("Server Initializied");
+
+        SpawnPlayer();
+    }
+
+
+    private void OnConnectedToServer()
+    {
+        Debug.Log("Server Joined");
+
+        SpawnPlayer();
+    }
+
+
+    private void OnGUI()
+    {
+        // We will draw a button to the screen if we have not started or joined a server
+        if(!Network.isClient && !Network.isServer)
+        {
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+                StartServer();
+
+            if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+                RefreshHostList();
+
+
+            if(hostList != null)
+            {
+                for(int i=0; i<hostList.Length; i++)
+                {
+                    if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+                        JoinServer(hostList[i]);
+                }
+            }
+
+        }
+    }
+
+
+    private void OnMasterServerEvent(MasterServerEvent msEvent)
+    {
+        // Check to see if the message that was recieved is our list of host data
+        if (msEvent == MasterServerEvent.HostListReceived)
+            hostList = MasterServer.PollHostList();
+    }
+
+
+
+
+    /**
+    *  FUNCTIONS
+    * */
+    private void StartServer()
+    {
+        // To Initialize you need to list the max players 4
+        // and the port number 25000
+        Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+        MasterServer.RegisterHost(TYPENAME, GAMENAME);
+    }
+
+
+    private void RefreshHostList()
+    {
+        MasterServer.RequestHostList(TYPENAME);
+    }
+
+
+    private void JoinServer(HostData hostData)
+    {
+        Network.Connect(hostData);
+    }
+
+
+    private void SpawnPlayer()
+    {
+        Network.Instantiate(playerPrefab, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    /**
+    *  VARIABLES
+    * */
+    public float speed = 10f;
+
+
+    private Rigidbody myRigidbody;
+
+
+
+
+    /**
+    *  CLASS FUNCTIONS
+    * */
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void Update()
+    {
+        InputMovement();
+    }
+
+
+
+
+
+
+
+
+    /**
+    *  FUNCTIONS
+    * */
+    private void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(transform.position + Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(transform.position - Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(transform.position + Vector3.right * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(transform.position - Vector3.right * speed * Time.deltaTime);
+    }
+}
