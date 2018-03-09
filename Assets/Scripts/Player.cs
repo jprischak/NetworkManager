5419dde64d4158cced12ef56c9ce7d2cafdcf19d
jprischak <jprischak@gmail.com>
2018-03-09 18:07:02 -0500

    Finished Tutorial

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,8 +10,16 @@ public class Player : MonoBehaviour {
     public float speed = 10f;
 
 
-    //private Rigidbody myRigidbody;
+   
     private NetworkView networkView;
+    private Rigidbody myRigidBody;
+    private float lastSynchronizationTime = 0f;
+    private float syncDelay = 0f;
+    private float syncTime = 0f;
+    private Vector3 syncStartPosition = Vector3.zero;
+    private Vector3 syncEndPosition = Vector3.zero;
+
+
 
 
 
@@ -20,33 +28,51 @@ public class Player : MonoBehaviour {
     * */
     private void Start()
     {
-        //myRigidbody = GetComponent<Rigidbody>();
         networkView = GetComponent<NetworkView>();
+        myRigidBody = GetComponent<Rigidbody>();
     }
 
 
     private void Update()
     {
-        //if(networkView.isMine)
+        if (networkView.isMine)
             InputMovement();
+        else
+            SyncMovement();
     }
 
 
-    //private void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
-    //{
-    //    Vector3 syncPosition = Vector3.zero;
+    private void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        Vector3 syncPosition = Vector3.zero;
+        Vector3 syncVelocity = Vector3.zero;
 
-    //    if(stream.isWriting)
-    //    {
-    //        syncPosition = transform.position;
-    //        stream.Serialize(ref syncPosition);
-    //    }
-    //    else
-    //    {
-    //        stream.Serialize(ref syncPosition);
-    //        transform.position = syncPosition;
-    //    }
-    //}
+
+
+        if (stream.isWriting)
+        {
+            // When adding items to the stream the order is important, it will be read in the same order
+            syncPosition = transform.position;
+            stream.Serialize(ref syncPosition);
+
+            syncVelocity = myRigidBody.velocity;
+            stream.Serialize(ref syncVelocity);
+        }
+        else
+        {
+            // Read our values back from the stream
+            stream.Serialize(ref syncPosition);
+            stream.Serialize(ref syncVelocity);
+
+
+            syncTime = 0f;
+            syncDelay = Time.time - lastSynchronizationTime;
+            lastSynchronizationTime = Time.time;
+
+            syncStartPosition = transform.position;
+            syncEndPosition = syncPosition + syncVelocity * syncDelay;
+        }
+    }
 
 
 
@@ -68,5 +94,12 @@ public class Player : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.A))
             transform.Translate(-Vector3.right * speed * Time.deltaTime);
+    }
+
+
+    private void SyncMovement()
+    {
+        syncTime += Time.deltaTime;
+        transform.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
     }
 }
